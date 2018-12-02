4e9b7eff2df67c677d188caf7489395ad3e28beb
Sonia-Florina Horchidan <sonia.florina@yahoo.com>
2018-12-02 14:46:46 +0100

    Fixed spacing

--- hw3-rmi-and-jdbc/src/common/FileDTO.java
+++ hw3-rmi-and-jdbc/src/common/FileDTO.java
@@ -4,14 +4,9 @@ import java.io.Serializable;
 
 public interface FileDTO extends Serializable {
     String getName();
-
     int getSize();
-
     String getOwner();
-
     boolean hasReadPermission();
-
     boolean hasWritePermission();
-
     String toString();
 }
